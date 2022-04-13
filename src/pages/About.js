88cc9ddc91d40948a4815c8660f9614765bcce73
ECHOSHIH <chunyushih@outlook.com>
2022-04-13 19:00:26 +0800

    [docs] add links

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { FaGithub } from 'react-icons/fa'
 
 const About = () => {
   return (
     <>
-      <h1 className='text-6xl mb-4'>Calculator</h1>
+      <h1 className='text-6xl mb-4'>
+        <a
+          href='https://github.com/EchoChunyuShih/simple-calculator'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Calculator <FaGithub className='inline' />
+        </a>
+      </h1>
       <p className='mb-4 text-2xl font-light'>
         A simple React calculator practice with
         <a href='https://www.youtube.com/watch?v=DgRrrOt0Vr8'>
@@ -15,7 +23,6 @@ const About = () => {
       <p className='text-lg text-gray-400'>
         Version <span className='text-white'>1.0.0</span>
       </p>
-      <p className='text-lg text-gray-400'>Source Code: {''}</p>
     </>
   )
 }
