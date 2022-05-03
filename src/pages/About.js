f82bd086da1652dd6af47e209aad9eccf330b4f9
Echo Shih <chunyushih@outlook.com>
2022-05-03 18:41:00 +0800

    [docs] edit about content

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,16 +13,13 @@ const About = () => {
         </a>
       </h1>
       <p className='mb-4 text-2xl font-light'>
-        A simple React calculator practice with
+        A simple React calculator inspired by
         <a href='https://www.youtube.com/watch?v=DgRrrOt0Vr8'>
           {' '}
           The Perfect Beginner React Project <br />
         </a>
-        by <strong>Web Dev Simplified</strong>.
       </p>
-      <p className='text-lg text-gray-400'>
-        Version <span className='text-white'>1.0.0</span>
-      </p>
+      <p className='text-lg text-gray-400'>Version 1.0.0</p>
     </>
   )
 }
